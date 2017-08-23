5b984dc26c47893cce7b6a2858b3cabe927a3af2
Dmitry Kolinchuk <dmko@sitecore.net>
2017-08-23 10:17:52 +0300

    Fixes the versions comparison issue in the CheckRevision processor

---
+++ src/Sitecore.Support.76398/ToggleLockRequest.cs
@@ -0,0 +1,51 @@
+﻿using Sitecore.Data.Items;
+using Sitecore.ExperienceEditor.Speak.Server.Contexts;
+using Sitecore.ExperienceEditor.Speak.Server.Requests;
+using Sitecore.ExperienceEditor.Speak.Server.Responses;
+using Sitecore.Web;
+using System;
+
+namespace Sitecore.Support.ExperienceEditor.Speak.Ribbon.Requests.LockItem
+{
+	public class ToggleLockRequest : PipelineProcessorRequest<ItemContext>
+	{
+		public override PipelineProcessorResponseValue ProcessRequest()
+		{
+			base.RequestContext.ValidateContextItem();
+			Item item = this.SwitchLock(base.RequestContext.Item);
+			this.HandleVersionCreating(item);
+			return new PipelineProcessorResponseValue
+			{
+				Value = new
+				{
+					Locked = item.Locking.IsLocked(),
+                    Version = item.Version.Number,
+                    Revision = item[FieldIDs.Revision]
+                }
+			};
+		}
+
+		protected Item SwitchLock(Item item)
+		{
+			if (item.Locking.IsLocked())
+			{
+				item.Locking.Unlock();
+				return item;
+			}
+			if (Sitecore.Context.User.IsAdministrator)
+			{
+				item.Locking.Lock();
+				return item;
+			}
+			return Sitecore.Context.Workflow.StartEditing(item);
+		}
+
+		private void HandleVersionCreating(Item finalItem)
+		{
+			if (base.RequestContext.Item.Version.Number != finalItem.Version.Number)
+			{
+				WebUtil.SetCookieValue(base.RequestContext.Site.GetCookieKey("sc_date"), string.Empty, DateTime.MinValue);
+			}
+		}
+	}
+}
---
+++ src/Sitecore.Support.76398/sitecore/shell/client/Sitecore/ExperienceEditor/Pipelines/LockItem/LockItem.Lock.js
@@ -0,0 +1,23 @@
+﻿define
+    (
+    [
+        "sitecore",
+        "/-/speak/v1/ExperienceEditor/ExperienceEditor.js",
+        "/-/speak/v1/ExperienceEditor/ExperienceEditor.Context.js"
+    ], function (Sitecore, ExperienceEditor, ExperienceEditorContext) {
+        return ExperienceEditor.PipelinesUtil.generateRequestProcessor("ExperienceEditor.LockItem", function (response) {
+            if (response.context.currentContext.version != response.responseValue.value.Version) {
+                response.context.app.refreshOnItem(response.context.currentContext, true);
+            }
+
+            var locked = response.responseValue.value.Locked;
+            // Sitecore.Support.76398
+            ExperienceEditor.getPageEditingWindow().Sitecore.PageModes.ChromeManager.updateItemRevision(response.context.currentContext.itemId, response.responseValue.value.Revision);
+
+            Sitecore.Commands.Lock.setButtonTitle(ExperienceEditorContext.instance, locked);
+            ExperienceEditorContext.instance.currentContext.isLocked = locked;
+            if (locked) {
+                ExperienceEditorContext.instance.currentContext.isLockedByCurrentUser = true;
+            }
+        });
+    });

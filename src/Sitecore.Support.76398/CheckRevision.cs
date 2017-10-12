e757aad4b6e8e4fca963efa91c65b1b1646f32b0
Dmitry Kolinchuk <dmko@sitecore.net>
2017-10-12 17:14:43 +0300

    Fix the CheckRevision processor

---
+++ src/Sitecore.Support.76398/CheckRevision.cs
@@ -0,0 +1,36 @@
+﻿using Sitecore.Data.Items;
+using Sitecore.ExperienceEditor.Speak.Server.Contexts;
+using Sitecore.ExperienceEditor.Speak.Server.Requests;
+using Sitecore.ExperienceEditor.Speak.Server.Responses;
+using Sitecore.Globalization;
+using Sitecore.Pipelines.Save;
+using System;
+
+namespace Sitecore.Support.ExperienceEditor.Speak.Ribbon.Requests.SaveItem
+{
+    public class CheckRevision : PipelineProcessorRequest<PageContext>
+    {
+        public override PipelineProcessorResponseValue ProcessRequest()
+        {
+            SaveArgs.SaveItem saveItem = base.RequestContext.GetSaveArgs().Items[0];
+            PipelineProcessorResponseValue pipelineProcessorResponseValue = new PipelineProcessorResponseValue();
+            //Item item = base.RequestContext.Item.Database.GetItem(saveItem.ID, Language.Parse(base.RequestContext.Language), Sitecore.Data.Version.Parse(base.RequestContext.Version));
+            Item item = base.RequestContext.Item.Database.GetItem(saveItem.ID, saveItem.Language, saveItem.Version);
+            if (item == null)
+            {
+                return pipelineProcessorResponseValue;
+            }
+            string text = item[FieldIDs.Revision].Replace("-", string.Empty);
+            if (saveItem.Revision == string.Empty)
+            {
+                saveItem.Revision = text;
+            }
+            string strB = saveItem.Revision.Replace("-", string.Empty);
+            if (string.Compare(text, strB, StringComparison.InvariantCultureIgnoreCase) != 0 && string.Compare("#!#Ignore revision#!#", strB, StringComparison.InvariantCultureIgnoreCase) != 0)
+            {
+                pipelineProcessorResponseValue.ConfirmMessage = Translate.Text("One or more items have been changed.\n\nDo you want to overwrite these changes?");
+            }
+            return pipelineProcessorResponseValue;
+        }
+    }
+}
--- src/Sitecore.Support.76398/ToggleLockRequest.cs
+++ src/Sitecore.Support.76398/ToggleLockRequest.cs
@@ -7,45 +7,45 @@ using System;
 
 namespace Sitecore.Support.ExperienceEditor.Speak.Ribbon.Requests.LockItem
 {
-	public class ToggleLockRequest : PipelineProcessorRequest<ItemContext>
-	{
-		public override PipelineProcessorResponseValue ProcessRequest()
-		{
-			base.RequestContext.ValidateContextItem();
-			Item item = this.SwitchLock(base.RequestContext.Item);
-			this.HandleVersionCreating(item);
-			return new PipelineProcessorResponseValue
-			{
-				Value = new
-				{
-					Locked = item.Locking.IsLocked(),
+    public class ToggleLockRequest : PipelineProcessorRequest<ItemContext>
+    {
+        public override PipelineProcessorResponseValue ProcessRequest()
+        {
+            base.RequestContext.ValidateContextItem();
+            Item item = this.SwitchLock(base.RequestContext.Item);
+            this.HandleVersionCreating(item);
+            return new PipelineProcessorResponseValue
+            {
+                Value = new
+                {
+                    Locked = item.Locking.IsLocked(),
                     Version = item.Version.Number,
                     Revision = item[FieldIDs.Revision]
                 }
-			};
-		}
+            };
+        }
 
-		protected Item SwitchLock(Item item)
-		{
-			if (item.Locking.IsLocked())
-			{
-				item.Locking.Unlock();
-				return item;
-			}
-			if (Sitecore.Context.User.IsAdministrator)
-			{
-				item.Locking.Lock();
-				return item;
-			}
-			return Sitecore.Context.Workflow.StartEditing(item);
-		}
+        protected Item SwitchLock(Item item)
+        {
+            if (item.Locking.IsLocked())
+            {
+                item.Locking.Unlock();
+                return item;
+            }
+            if (Sitecore.Context.User.IsAdministrator)
+            {
+                item.Locking.Lock();
+                return item;
+            }
+            return Sitecore.Context.Workflow.StartEditing(item);
+        }
 
-		private void HandleVersionCreating(Item finalItem)
-		{
-			if (base.RequestContext.Item.Version.Number != finalItem.Version.Number)
-			{
-				WebUtil.SetCookieValue(base.RequestContext.Site.GetCookieKey("sc_date"), string.Empty, DateTime.MinValue);
-			}
-		}
-	}
+        private void HandleVersionCreating(Item finalItem)
+        {
+            if (base.RequestContext.Item.Version.Number != finalItem.Version.Number)
+            {
+                WebUtil.SetCookieValue(base.RequestContext.Site.GetCookieKey("sc_date"), string.Empty, DateTime.MinValue);
+            }
+        }
+    }
 }
